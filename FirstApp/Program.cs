a9f175fb358693930e0e8d15f6f422a382f5b368
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-10 19:21:17 +0530

    First Program and very basics

---
+++ FirstApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("Hello\n");
+            //Single line Comment
+
+            /*
+             This 
+            is Multiline
+            Comment
+             */
+
+            // DataTypes
+
+            int num1 = 23;
+            string name = "Rishi";
+            double num2 = 2.3;
+            Console.WriteLine(num1);
+            Console.WriteLine(name);
+            Console.WriteLine(num2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
